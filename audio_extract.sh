b04ff674fc0aed35881696e9c2ba71a5746dae33
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-06-29 01:21:59 +0530

    Modified Script

--- audio_extract.sh
+++ audio_extract.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/1.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/1.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/1.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/128bits/1.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/2.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/128bits/2.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/3.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/128bits/3.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/4.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/128bits/4.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/5.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/128bits/5.mp3
+
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/1.mp4 -vn -ab 256 /home/rishabh/Documents/iAugmentor/test_data/audios/256bits/1.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/2.mp4 -vn -ab 256 /home/rishabh/Documents/iAugmentor/test_data/audios/256bits/2.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/3.mp4 -vn -ab 256 /home/rishabh/Documents/iAugmentor/test_data/audios/256bits/3.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/4.mp4 -vn -ab 256 /home/rishabh/Documents/iAugmentor/test_data/audios/256bits/4.mp3
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/5.mp4 -vn -ab 256 /home/rishabh/Documents/iAugmentor/test_data/audios/256bits/5.mp3

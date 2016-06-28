776b1c650853cfac6952964cfb5c623f3be3464e
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-06-29 01:07:25 +0530

    Added Extraction File

---
+++ audio_extract.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ffmpeg -i /home/rishabh/Documents/iAugmentor/test_data/1.mp4 -vn -ab 128 /home/rishabh/Documents/iAugmentor/test_data/audios/1.mp3
--- librosa.py
+++ librosa.py
@@ -1,14 +1,13 @@
-# Beat tracking example
+# Beat tracking example	
 from __future__ import print_function
 import librosa
 
 # 1. Get the file path to the included audio example
-filename = '../test.wav'
-
+filename = '/home/rishabh/Documents/iAugmentor/AudioAnalysis/audio/1mp4.mp3'
 # 2. Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
-y, sr = librosa.load(filename)
-
+y, sr = librosa.core.load(filename, sr=22050)
+'''
 # 3. Run the default beat tracker
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
@@ -19,3 +18,4 @@ beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
 print('Saving output to beat_times.csv')
 librosa.output.times_csv('beat_times.csv', beat_times)
+'''

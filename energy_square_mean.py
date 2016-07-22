c0448fac6be9467b28a4caea01c0d226bb9a2ae4
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-07-22 12:56:07 +0530

    Working mp3 code added.

--- energy_square_mean.py
+++ energy_square_mean.py
@@ -6,16 +6,16 @@ import math
 from os import listdir
 import matplotlib.pyplot as plt
 
-base_path_loud = "/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/loud5/"
-base_path_no_loud = "/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/no_loud4/"
+base_path_loud = "/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/loud5/mp3/"
+base_path_no_loud = "/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/no_loud4/mp3/"
 
 
-loud_audios = listdir('/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/loud5')
-no_loud_audios = listdir('/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/no_loud4')
+loud_audios = listdir('/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/loud5/mp3')
+no_loud_audios = listdir('/home/rishabh/Documents/iAugmentor/AudioAnalysis/scripts/no_loud4/mp3')
 
 for audio in loud_audios :
-	fs, data = librosa.read(base_path_loud+audio)
-	fs, data = wavfile.read(base_path_loud+audio)
+	data, fs = librosa.load(base_path_loud+audio)
+	# fs, data = wavfile.read(base_path_loud+audio)
 	# np.savetxt(str(audio)+".txt", data)
 	absolute_value = np.absolute(data, dtype='float32') # calculate absolute values of channels
 	data_square = absolute_value**2
@@ -24,11 +24,11 @@ for audio in loud_audios :
 	print "Loud Audio value : "+ str(audio)+ "  : ", mean_value
 	# print "squared data datatype",data_square.dtype
 	# # print "mean data datatype", mean_value.dtype
-	# plt.plot(absolute_value)
-	# plt.xlabel("Sampling rate")
-	# plt.ylabel("Amplitude")
-	# plt.title("Loud Audio"+str(audio))
-	# plt.show()
+	plt.plot(data_square)
+	plt.xlabel("Sampling rate")
+	plt.ylabel("Amplitude")
+	plt.title("Loud Audio"+str(audio))
+	plt.show()
 	# plt.savefig(str(audio.split(".")[0]), format="jpeg")
 
 print "**********************************************************"
@@ -36,16 +36,16 @@ print "=========================================================="
 
 
 for audio in no_loud_audios:
-	fs, data = librosa.read(base_path_no_loud+audio)
-	fs, data = wavfile.read(base_path_no_loud + audio) # load the data
+	data, fs = librosa.load(base_path_no_loud+audio)
+	# fs, data = wavfile.read(base_path_no_loud + audio) # load the data
 	absolute_value = np.absolute(data, dtype='float32') # calculate absolute values of channels
 	data_square = np.square(absolute_value)
 	# np.savetxt(str(audio)+"no_loud.txt", data_square)
 	mean_value = np.mean(data_square)
 	print "Not Loud Audio value  :"+ str(audio)+ "  : ", mean_value
-	# plt.plot(absolute_value)
-	# plt.xlabel("Sampling Rate")
-	# plt.ylabel("Amplitude")
-	# plt.title("Not Loud"+str(audio))
-	# plt.show()
+	plt.plot(data_square)
+	plt.xlabel("Sampling Rate")
+	plt.ylabel("Amplitude")
+	plt.title("Not Loud"+str(audio))
+	plt.show()
 	# plt.savefig(str(audio.split(".")[0]), format="jpeg")
